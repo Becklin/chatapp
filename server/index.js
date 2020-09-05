b9f5ab2d60c53dc70b1ef0d1d7a9e18b39d946a9
beck <stevenaura@yahoo.com.tw>
2020-09-05 10:55:01 +0800

    add socket stream

--- server/index.js
+++ server/index.js
@@ -19,20 +19,44 @@ const {
 io.on('connection', socket => {
   console.log('we have connection!!!');
 
+  // 方法零 一次整個傳輸
   //   fs.readFile(__dirname + '/images/img1.jpg', function(err, buf) {
   //     console.log(buf);
   //     socket.emit('image', { image: true, buffer: buf.toString('base64') });
   //     console.log('image file is initialized');
   //   });
-  // let medium = '';
-  // var readableStream = fs.createReadStream('../DigitalGlobe_QuickBird_60cm_8bit_RGB_DRA_Boulder_2005JUL04_8bits_sub_r_1.jpg');
-  // readableStream.on('data', function(chunk) {
-  //   console.log('強克', chunk);
-  //   medium+=chunk;
+
+  // 方法一利用pipepipe就是寫進去的動作
+  // let readableStream = fs.createReadStream('./images/img2.jpg');
+  // let writableStream = fs.createWriteStream('./images/testimg2.jpg');
+  // readableStream.on('data', function (chunk) {
+  //   console.log(chunk);
+  // })
+  // readableStream.pipe(writableStream);
+  // readableStream.on('end', function(chunk) {
+  //   console.log('結束');
   // });
-  // readableStream.on('end', function() {
-  //     console.log(medium);
+
+  // 方法二
+  // var sourceStream = fs.createReadStream('./images/img1.jpg');
+  // var sinkStream = fs.createWriteStream('./images/testimg1.jpg');
+  // sourceStream.on('data', function (chunk) {
+  //   console.log(chunk);
+  //   sinkStream.write(chunk);
+  // }).on('end', function () {
+  //   sinkStream.end();
   // });
+
+  //來玩玩socket.io-stream
+  ss(socket).on('sendFile', function(stream, data) {
+    console.log('data', data);
+    var filename = path.basename(data.name);
+    stream.pipe(fs.createWriteStream(filename));
+  });
+
+
+
+
 
   socket.on('join', ({ name, room }, errorCallback) => {
     const { error, user } = addUser({ id: socket.id, name, room });
@@ -90,13 +114,16 @@ io.on('connection', socket => {
   //   });
   //   callback();
   // });
-  ss(socket).on('sendFile', (stream, data) => {
+  ss(socket).on('sendFile_bak', (stream, data) => {
     console.log('data', data);
     console.log('stream', stream);
     const user = getUser(socket.id);
     //io.to要查
     const filename = path.basename(data.name);
     console.log('filename', filename);
+    stream.on('data', (chunk) => {
+      console.log(chunk);
+    })
     stream.pipe(fs.createWriteStream(filename));
     // io.to(user.room).emit('file', {
     //   user: user.name,

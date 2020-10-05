1a9aa269b536fb63ed7ac863ebf30f82dbe16850
BeckLin <beck.lin@ISTWLAPAPPLE2029.local>
2020-10-05 14:10:18 +0800

    lint

--- server/index.js
+++ server/index.js
@@ -1,33 +1,33 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
-const socketio = require("socket.io");
-var ss = require("socket.io-stream");
-const http = require("http");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const fs = require("fs");
-const path = require("path");
+const express = require('express');
+const socketio = require('socket.io');
+var ss = require('socket.io-stream');
+const http = require('http');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
 const PORT = process.env.PORT || 5000;
-const router = require("./routes/main.routes");
+const router = require('./routes/main.routes');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Origin', '*');
   res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
   );
 
   // Add this
-  if (req.method === "OPTIONS") {
+  if (req.method === 'OPTIONS') {
     res.header(
-      "Access-Control-Allow-Methods",
-      "PUT, POST, PATCH, DELETE, OPTIONS"
+      'Access-Control-Allow-Methods',
+      'PUT, POST, PATCH, DELETE, OPTIONS'
     );
-    res.header("Access-Control-Max-Age", 120);
+    res.header('Access-Control-Max-Age', 120);
     return res.status(200).json({});
   }
   next();
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
 // provides Express middleware to enable CORS
 const corsOptions = {
   // origin: `http://localhost:${PORT}`
-  origin: "http://localhost:3000",
+  origin: 'http://localhost:3000'
 };
 // provides Express middleware to enable CORS
 app.use(cors(corsOptions));
@@ -47,26 +47,26 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-require("./routes/auth.routes")(app);
-require("./routes/user.routes")(app);
-require("./routes/main.routes")(app);
-const app_setting = require("./app.json");
-console.log("app_setting", app_setting);
+require('./routes/auth.routes')(app);
+require('./routes/user.routes')(app);
+require('./routes/main.routes')(app);
+const app_setting = require('./app.json') || '';
+console.log('app_setting', app_setting);
 const uri = `mongodb+srv://beckLin:${app_setting.MONGO_PW}@cluster1.juqcg.mongodb.net/${app_setting.MONGO_DB}?retryWrites=true&w=majority`;
-console.log(" uri", uri);
-const db = require("./models");
+console.log(' uri', uri);
+const db = require('./models');
 const Role = db.role;
 db.mongoose
   .connect(uri, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+    useUnifiedTopology: true
   })
   .then(() => {
-    console.log("Successfully connect to MongoDB.");
+    console.log('Successfully connect to MongoDB.');
     initial();
   })
-  .catch((err) => {
-    console.error("Connection error", err);
+  .catch(err => {
+    console.error('Connection error', err);
     process.exit();
   });
 
@@ -76,31 +76,31 @@ const initial = () => {
   Role.estimatedDocumentCount((err, count) => {
     if (!err && count === 0) {
       new Role({
-        name: "user",
-      }).save((err) => {
+        name: 'user'
+      }).save(err => {
         // create a new User: object.save()
         if (err) {
-          console.log("error", err);
+          console.log('error', err);
         }
 
         console.log("added 'user' to roles collection");
       });
 
       new Role({
-        name: "moderator",
-      }).save((err) => {
+        name: 'moderator'
+      }).save(err => {
         if (err) {
-          console.log("error", err);
+          console.log('error', err);
         }
 
         console.log("added 'moderator' to roles collection");
       });
 
       new Role({
-        name: "admin",
-      }).save((err) => {
+        name: 'admin'
+      }).save(err => {
         if (err) {
-          console.log("error", err);
+          console.log('error', err);
         }
 
         console.log("added 'admin' to roles collection");
@@ -109,10 +109,10 @@ const initial = () => {
   });
 };
 
-const AWS = require("aws-sdk");
+const AWS = require('aws-sdk');
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY,
-  accessKeyId: process.env.AWS_ACCESS_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY
 });
 // let myBucket = 'eazychat';
 // let myKey = 'test';
@@ -132,9 +132,9 @@ const s3 = new AWS.S3({
 //   }
 // });
 
-const { addUser, getUser, removeUser, getUsersInRoom } = require("./users");
-io.on("connection", (socket) => {
-  console.log("we have connection!!!");
+const { addUser, getUser, removeUser, getUsersInRoom } = require('./users');
+io.on('connection', socket => {
+  console.log('we have connection!!!');
 
   // 方法零 一次整個傳輸
   //   fs.readFile(__dirname + '/images/img1.jpg', function(err, buf) {
@@ -171,47 +171,47 @@ io.on("connection", (socket) => {
   //   stream.pipe(fs.createWriteStream(filename));
   // });
 
-  socket.on("join", ({ name, room }, errorCallback) => {
+  socket.on('join', ({ name, room }, errorCallback) => {
     const { error, user } = addUser({ id: socket.id, name, room });
     if (error) return errorCallback(error);
     socket.join(user.room);
-    socket.emit("message", {
-      user: "admin",
-      text: `${user.name}, welcome to the room ${user.room}`,
+    socket.emit('message', {
+      user: 'admin',
+      text: `${user.name}, welcome to the room ${user.room}`
     });
 
-    socket.emit("image", {
-      user: "admin",
-      text: `${user.name}, welcome to the room ${user.room}`,
+    socket.emit('image', {
+      user: 'admin',
+      text: `${user.name}, welcome to the room ${user.room}`
     });
 
     // broadcast: send message to everyone besides to that user
     socket.broadcast
       .to(user.room)
-      .emit("message", { user: "admin", text: `${user.name} has joined!` });
+      .emit('message', { user: 'admin', text: `${user.name} has joined!` });
     //要查
-    io.to(user.room).emit("roomData", {
+    io.to(user.room).emit('roomData', {
       room: user.room,
-      users: getUsersInRoom(user.room),
+      users: getUsersInRoom(user.room)
     });
     errorCallback();
   });
-  socket.on("sendMessage", (message, callback) => {
+  socket.on('sendMessage', (message, callback) => {
     const user = getUser(socket.id);
-    const isGoogleTyping = message.includes("@gg=");
+    const isGoogleTyping = message.includes('@gg=');
     if (isGoogleTyping) {
-      const destination = message.split("@gg=").pop();
+      const destination = message.split('@gg=').pop();
       const addressDom = `<a target="blank" href='https://www.google.com.tw/maps/search/${destination}'>${destination}</a>`;
-      io.to(user.room).emit("message", {
+      io.to(user.room).emit('message', {
         user: user.name,
         text: null,
-        address: addressDom,
+        address: addressDom
       });
       callback(); //奇怪
       return;
     }
     //io.to要查
-    io.to(user.room).emit("message", { user: user.name, text: message });
+    io.to(user.room).emit('message', { user: user.name, text: message });
     // io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
     callback(); //奇怪
   });
@@ -233,9 +233,9 @@ io.on("connection", (socket) => {
 
   const uploadFileToAws = (bufferData, fileName, userName) => {
     const params = {
-      Bucket: "easychat", // pass your bucket name
+      Bucket: 'easychat', // pass your bucket name
       Key: fileName, // file will be saved as testBucket/contacts.csv
-      Body: bufferData, //JSON.stringify(data, null, 2)
+      Body: bufferData //JSON.stringify(data, null, 2)
     };
     s3.upload(params, function (s3Err, data) {
       if (s3Err) throw s3Err;
@@ -243,26 +243,26 @@ io.on("connection", (socket) => {
     });
   };
 
-  ss(socket).on("sendFile", (stream, data, callback) => {
+  ss(socket).on('sendFile', (stream, data, callback) => {
     const user = getUser(socket.id);
     //io.to要查
     // const filename = path.basename(data.name);
     let size = 0;
     let fileBuffer = [];
-    stream.on("data", (chunk) => {
+    stream.on('data', chunk => {
       size += chunk.length;
-      console.log(Math.floor((size / data.size) * 100) + "%");
+      console.log(Math.floor((size / data.size) * 100) + '%');
       fileBuffer.push(chunk);
     });
-    stream.on("end", () => {
+    stream.on('end', () => {
       /* TODO 以上會在上傳到aws，上傳前直接在前端把圖檔preview就好，以下可以不用作
     右邊為轉成webP技巧網站 https://css-tricks.com/using-webp-images/ */
       // stream.on("end", () => {
-      const sentFile = Buffer.concat(fileBuffer).toString("base64");
-      io.to(user.room).emit("file", {
+      const sentFile = Buffer.concat(fileBuffer).toString('base64');
+      io.to(user.room).emit('file', {
         user: user.name,
         upload: sentFile,
-        type: data.type,
+        type: data.type
       });
     });
 
@@ -278,18 +278,18 @@ io.on("connection", (socket) => {
     callback && callback();
   });
 
-  ss(socket).on("uploadFile", (stream, data, callback) => {
+  ss(socket).on('uploadFile', (stream, data, callback) => {
     const user = getUser(socket.id);
     //io.to要查
     // const filename = path.basename(data.name);
     let size = 0;
     let fileBuffer = [];
-    stream.on("data", (chunk) => {
+    stream.on('data', chunk => {
       size += chunk.length;
-      console.log(Math.floor((size / data.size) * 100) + "%");
+      console.log(Math.floor((size / data.size) * 100) + '%');
       fileBuffer.push(chunk);
     });
-    stream.on("end", () => {
+    stream.on('end', () => {
       const BufferData = Buffer.concat(fileBuffer);
       uploadFileToAws(BufferData, data.name, user.name);
       /* TODO 以上會在上傳到aws，上傳前直接在前端把圖檔preview就好，以下可以不用作
@@ -307,19 +307,19 @@ io.on("connection", (socket) => {
     // });
     callback && callback();
   });
-  socket.on("disconnect", () => {
-    console.log("disconnect!!!");
+  socket.on('disconnect', () => {
+    console.log('disconnect!!!');
     const user = removeUser(socket.id);
     if (user) {
-      io.to(user.room).emit("message", {
-        user: "admin",
-        text: `${user.name} has left.`,
+      io.to(user.room).emit('message', {
+        user: 'admin',
+        text: `${user.name} has left.`
       });
     }
   });
 });
 
 server.listen(PORT, () => {
-  console.log("2=", PORT);
+  console.log('2=', PORT);
   console.log(`listening ${PORT}`);
 });

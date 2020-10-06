04799ce117fb5a927a91eb6e735ced3cb65b8108
BeckLin <beck.lin@ISTWLAPAPPLE2029.local>
2020-10-06 16:10:52 +0800

    adjust css

--- client/src/components/Input/index.js
+++ client/src/components/Input/index.js
@@ -8,31 +8,25 @@ const Input = ({ message, setMessage, sendMessage, sendFile, uploadFile }) => {
   const handleSendMessage = (event) => sendMessage(event);
   return (
     <form className="chat__form">
-      <Row>
-        <Col xs={12}>
-          <input
-            className="chat__input"
-            value={message}
-            onChange={(event) => setMessage(event.target.value)}
-            onKeyPress={(event) =>
-              event.key === 'Enter' ? setMessage(event.target.value) : null
-            }
-          />
-        </Col>
-        <Col xs={12}>
-          <FileButton size="sm" type="sendFile" onHandleFile={sendFile}>
-            Files
-          </FileButton>
-          <FileButton size="sm" type="upload" onHandleFile={uploadFile}>
-            Files
-          </FileButton>
-          <ChatLeftTextFill
-            size="32"
-            onClick={message ? handleSendMessage : null}
-            color={message ? 'white' : 'gray'}
-          />
-        </Col>
-      </Row>
+      <input
+        className="chat__input"
+        value={message}
+        onChange={(event) => setMessage(event.target.value)}
+        onKeyPress={(event) =>
+          event.key === 'Enter' ? setMessage(event.target.value) : null
+        }
+      />
+      <FileButton size="sm" type="sendFile" onHandleFile={sendFile}>
+        Files
+      </FileButton>
+      <FileButton size="sm" type="upload" onHandleFile={uploadFile}>
+        Files
+      </FileButton>
+      <ChatLeftTextFill
+        size="32"
+        onClick={message ? handleSendMessage : null}
+        color={message ? 'white' : 'gray'}
+      />
     </form>
   );
 };

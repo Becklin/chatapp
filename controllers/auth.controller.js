df7e8cd745721e101f5d694e5fc5abb2a99ef9dc
beck <stevenaura@yahoo.com.tw>
2020-10-10 13:47:56 +0800

    fix login err

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,16 +1,16 @@
-const config = require('../config/auth.config');
-const db = require('../models');
+const config = require("../config/auth.config");
+const db = require("../models");
 const User = db.user;
 const Role = db.role;
 
-var jwt = require('jsonwebtoken');
-var bcrypt = require('bcryptjs');
+var jwt = require("jsonwebtoken");
+var bcrypt = require("bcryptjs");
 
 exports.signup = (req, res) => {
   const user = new User({
     username: req.body.username,
     email: req.body.email,
-    password: bcrypt.hashSync(req.body.password, 8)
+    password: bcrypt.hashSync(req.body.password, 8),
   });
 
   user.save((err, user) => {
@@ -22,39 +22,39 @@ exports.signup = (req, res) => {
     if (req.body.roles) {
       Role.find(
         {
-          name: { $in: req.body.roles }
+          name: { $in: req.body.roles },
         },
         (err, roles) => {
           if (err) {
             res.status(500).send({ message: err });
             return;
           }
-          user.roles = roles.map(role => role._id);
-          user.save(err => {
+          user.roles = roles.map((role) => role._id);
+          user.save((err) => {
             if (err) {
               res.status(500).send({ message: err });
               return;
             }
 
-            res.send({ message: 'User was registered successfully!' });
+            res.send({ message: "User was registered successfully!" });
           });
         }
       );
     } else {
-      Role.findOne({ name: 'user' }, (err, role) => {
+      Role.findOne({ name: "user" }, (err, role) => {
         if (err) {
           res.status(500).send({ message: err });
           return;
         }
 
         user.roles = [role._id];
-        user.save(err => {
+        user.save((err) => {
           if (err) {
             res.status(500).send({ message: err });
             return;
           }
 
-          res.send({ message: 'User was registered successfully!' });
+          res.send({ message: "User was registered successfully!" });
         });
       });
     }
@@ -62,16 +62,8 @@ exports.signup = (req, res) => {
 };
 
 exports.signin = (req, res) => {
-  res.status(200).send({
-    id: 'fdwacdawca',
-    username: req.body.username,
-    email: req.body.email,
-    roles: 'user',
-    accessToken: 'fewrgwaeva'
-  });
-
   User.findOne({
-    username: req.body.username
+    username: req.body.username,
   })
     /**
      * 在定義Schema的時候，如果設定某個 field 關聯另一個Schema，
@@ -79,14 +71,14 @@ exports.signin = (req, res) => {
      * 並且用被關聯 document 的內容替換掉原來關聯欄位(field)的內容。
      * https://codertw.com/%E8%B3%87%E6%96%99%E5%BA%AB/18236/
      */
-    .populate('roles', '-__v')
+    .populate("roles", "-__v")
     .exec((err, user) => {
       if (err) {
         res.status(500).send({ message: err });
         return;
       }
       if (!user) {
-        return res.status(404).send({ message: 'User Not found.' });
+        return res.status(404).send({ message: "User Not found." });
       }
 
       var passwordIsValid = bcrypt.compareSync(
@@ -97,25 +89,25 @@ exports.signin = (req, res) => {
       if (!passwordIsValid) {
         return res.status(401).send({
           accessToken: null,
-          message: 'Invalid Password!'
+          message: "Invalid Password!",
         });
       }
 
       var token = jwt.sign({ id: user.id }, config.secret, {
-        expiresIn: 86400 // 24 hours
+        expiresIn: 86400, // 24 hours
       });
 
       var authorities = [];
 
       for (let i = 0; i < user.roles.length; i++) {
-        authorities.push('ROLE_' + user.roles[i].name.toUpperCase());
+        authorities.push("ROLE_" + user.roles[i].name.toUpperCase());
       }
       res.status(200).send({
         id: user._id,
         username: user.username,
         email: user.email,
         roles: authorities,
-        accessToken: token
+        accessToken: token,
       });
     });
 };

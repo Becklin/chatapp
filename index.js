f088edf4ec513c6bd9dbea4460f702d6fccc1f80
BeckLin <beck.lin@ISTWLAPAPPLE2029.local>
2020-10-14 16:27:51 +0800

    add cluster workaround

--- index.js
+++ index.js
@@ -1,72 +1,56 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const cluster = require("cluster");
-const os = require("os");
-
-// is the file being executed in mster mode?
-if (cluster.isMaster) {
+const sticky = require('sticky-session');
+const cluster = require('cluster');
+const os = require('os');
+const masterProcess = () => {
   // this is for the master process that is started by calling
   // this script with node from the command line
-
   // Calling the os.cpus method will give an array of objects
   // with some basic info on the numder of cpus
   // on the system
   let cpus = os.cpus();
-
-  console.log("master: I am the master process.");
+  console.log('master: I am the master process.');
   // for each cpu
   cpus.forEach(function (cpu, i) {
-    console.log("master: forking a child process for cpu " + i);
-
+    console.log('master: forking a child process for cpu ' + i);
     // fork this script to a new worker by calling cluster.fork
     // this will return an instance of Worker
     let worker = cluster.fork();
-
-    // I can set some events for the worker here if I want
-    worker.on("exit", function () {
-      console.log("child: All done.");
-    });
   });
-  // the exit event will fire each time
-  // a worker exits
-  cluster.on("exit", function () {
-    let activeWorkers = Object.keys(cluster.workers).length;
+  cluster.fork();
+  process.exit();
+};
 
-    if (activeWorkers === 0) {
-      console.log("master: My work is done.");
-      process.exit();
-    }
-  });
-} else {
+const childProcess = () => {
   // Im a child, Im going to act like a server and do nothing else
-  const express = require("express");
-  const socketio = require("socket.io");
-  var ss = require("socket.io-stream");
-  const http = require("http");
-  const bodyParser = require("body-parser");
-  const cors = require("cors");
-  const fs = require("fs");
-  const path = require("path");
+  const express = require('express');
+  const socketio = require('socket.io');
+  var ss = require('socket.io-stream');
+  const http = require('http');
+  const bodyParser = require('body-parser');
+  const cors = require('cors');
+  const fs = require('fs');
+  const path = require('path');
   const PORT = process.env.PORT || 5000;
   const app = express();
   const server = http.createServer(app);
   const io = socketio(server);
-  const AppError = require("./utils/AppError");
+  const AppError = require('./utils/AppError');
 
   app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Origin', '*');
     res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
     );
-
     next();
   });
 
   // provides Express middleware to enable CORS
   const corsOptions = {
     // origin: `http://localhost:${PORT}`
-    origin: "http://localhost:3000",
+    origin: 'http://localhost:3000'
   };
   // provides Express middleware to enable CORS
   app.use(cors(corsOptions));
@@ -77,34 +61,34 @@ if (cluster.isMaster) {
   // parse requests of content-type - application/x-www-form-urlencoded
   app.use(bodyParser.urlencoded({ extended: true }));
 
-  if (process.env.NODE_ENV === "production") {
+  if (process.env.NODE_ENV === 'production') {
     // Serve any static files
-    console.log("__dirname", __dirname);
-    app.use(express.static(path.join(__dirname, "client/build")));
+    console.log('__dirname', __dirname);
+    app.use(express.static(path.join(__dirname, 'client/build')));
     // Handle React routing, return all requests to React app
-    app.get("*", function (req, res) {
-      console.log("全部__dirname", __dirname);
-      res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    app.get('*', function (req, res) {
+      console.log('全部__dirname', __dirname);
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
   }
 
-  require("./routes/index.routes")(app);
+  require('./routes/index.routes')(app);
 
   const uri = `mongodb+srv://beckLin:${process.env.MONGO_PW}@cluster1.juqcg.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
-  console.log(" uri", uri);
-  const db = require("./models");
+  console.log(' uri', uri);
+  const db = require('./models');
   const Role = db.role;
   db.mongoose
     .connect(uri, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
+      useUnifiedTopology: true
     })
     .then(() => {
-      console.log("Successfully connect to MongoDB.");
+      console.log('Successfully connect to MongoDB.');
       initial();
     })
-    .catch((err) => {
-      console.error("Connection error", err);
+    .catch(err => {
+      console.error('Connection error', err);
       process.exit();
     });
 
@@ -114,31 +98,31 @@ if (cluster.isMaster) {
     Role.estimatedDocumentCount((err, count) => {
       if (!err && count === 0) {
         new Role({
-          name: "user",
-        }).save((err) => {
+          name: 'user'
+        }).save(err => {
           // create a new User: object.save()
           if (err) {
-            console.log("error", err);
+            console.log('error', err);
           }
 
           console.log("added 'user' to roles collection");
         });
 
         new Role({
-          name: "moderator",
-        }).save((err) => {
+          name: 'moderator'
+        }).save(err => {
           if (err) {
-            console.log("error", err);
+            console.log('error', err);
           }
 
           console.log("added 'moderator' to roles collection");
         });
 
         new Role({
-          name: "admin",
-        }).save((err) => {
+          name: 'admin'
+        }).save(err => {
           if (err) {
-            console.log("error", err);
+            console.log('error', err);
           }
 
           console.log("added 'admin' to roles collection");
@@ -147,10 +131,10 @@ if (cluster.isMaster) {
     });
   };
 
-  const AWS = require("aws-sdk");
+  const AWS = require('aws-sdk');
   const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY,
-    accessKeyId: process.env.AWS_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY
   });
   // let myBucket = 'eazychat';
   // let myKey = 'test';
@@ -170,9 +154,9 @@ if (cluster.isMaster) {
   //   }
   // });
 
-  const { addUser, getUser, removeUser, getUsersInRoom } = require("./users");
-  io.on("connection", (socket) => {
-    console.log("we have connection!!!");
+  const { addUser, getUser, removeUser, getUsersInRoom } = require('./users');
+  io.on('connection', socket => {
+    console.log('we have connection!!!');
 
     // 方法零 一次整個傳輸
     //   fs.readFile(__dirname + '/images/img1.jpg', function(err, buf) {
@@ -209,47 +193,47 @@ if (cluster.isMaster) {
     //   stream.pipe(fs.createWriteStream(filename));
     // });
 
-    socket.on("join", ({ name, room }, errorCallback) => {
+    socket.on('join', ({ name, room }, errorCallback) => {
       const { error, user } = addUser({ id: socket.id, name, room });
       if (error) return errorCallback(error);
       socket.join(user.room);
-      socket.emit("message", {
-        user: "admin",
-        text: `${user.name}, welcome to the room ${user.room}`,
+      socket.emit('message', {
+        user: 'admin',
+        text: `${user.name}, welcome to the room ${user.room}`
       });
 
-      socket.emit("image", {
-        user: "admin",
-        text: `${user.name}, welcome to the room ${user.room}`,
+      socket.emit('image', {
+        user: 'admin',
+        text: `${user.name}, welcome to the room ${user.room}`
       });
 
       // broadcast: send message to everyone besides to that user
       socket.broadcast
         .to(user.room)
-        .emit("message", { user: "admin", text: `${user.name} has joined!` });
+        .emit('message', { user: 'admin', text: `${user.name} has joined!` });
       //要查
-      io.to(user.room).emit("roomData", {
+      io.to(user.room).emit('roomData', {
         room: user.room,
-        users: getUsersInRoom(user.room),
+        users: getUsersInRoom(user.room)
       });
       errorCallback();
     });
-    socket.on("sendMessage", (message, callback) => {
+    socket.on('sendMessage', (message, callback) => {
       const user = getUser(socket.id);
-      const isGoogleTyping = message.includes("@gg=");
+      const isGoogleTyping = message.includes('@gg=');
       if (isGoogleTyping) {
-        const destination = message.split("@gg=").pop();
+        const destination = message.split('@gg=').pop();
         const addressDom = `<a target="blank" href='https://www.google.com.tw/maps/search/${destination}'>${destination}</a>`;
-        io.to(user.room).emit("message", {
+        io.to(user.room).emit('message', {
           user: user.name,
           text: null,
-          address: addressDom,
+          address: addressDom
         });
         callback(); //奇怪
         return;
       }
       //io.to要查
-      io.to(user.room).emit("message", { user: user.name, text: message });
+      io.to(user.room).emit('message', { user: user.name, text: message });
       // io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
       callback(); //奇怪
     });
@@ -271,9 +255,9 @@ if (cluster.isMaster) {
 
     const uploadFileToAws = (bufferData, fileName, userName) => {
       const params = {
-        Bucket: "easychat", // pass your bucket name
+        Bucket: 'easychat', // pass your bucket name
         Key: fileName, // file will be saved as testBucket/contacts.csv
-        Body: bufferData, //JSON.stringify(data, null, 2)
+        Body: bufferData //JSON.stringify(data, null, 2)
       };
       s3.upload(params, function (s3Err, data) {
         if (s3Err) throw s3Err;
@@ -281,26 +265,26 @@ if (cluster.isMaster) {
       });
     };
 
-    ss(socket).on("sendFile", (stream, data, callback) => {
+    ss(socket).on('sendFile', (stream, data, callback) => {
       const user = getUser(socket.id);
       //io.to要查
       // const filename = path.basename(data.name);
       let size = 0;
       let fileBuffer = [];
-      stream.on("data", (chunk) => {
+      stream.on('data', chunk => {
         size += chunk.length;
-        console.log(Math.floor((size / data.size) * 100) + "%");
+        console.log(Math.floor((size / data.size) * 100) + '%');
         fileBuffer.push(chunk);
       });
-      stream.on("end", () => {
+      stream.on('end', () => {
         /* TODO 以上會在上傳到aws，上傳前直接在前端把圖檔preview就好，以下可以不用作
     右邊為轉成webP技巧網站 https://css-tricks.com/using-webp-images/ */
         // stream.on("end", () => {
-        const sentFile = Buffer.concat(fileBuffer).toString("base64");
-        io.to(user.room).emit("file", {
+        const sentFile = Buffer.concat(fileBuffer).toString('base64');
+        io.to(user.room).emit('file', {
           user: user.name,
           upload: sentFile,
-          type: data.type,
+          type: data.type
         });
       });
 
@@ -316,18 +300,18 @@ if (cluster.isMaster) {
       callback && callback();
     });
 
-    ss(socket).on("uploadFile", (stream, data, callback) => {
+    ss(socket).on('uploadFile', (stream, data, callback) => {
       const user = getUser(socket.id);
       //io.to要查
       // const filename = path.basename(data.name);
       let size = 0;
       let fileBuffer = [];
-      stream.on("data", (chunk) => {
+      stream.on('data', chunk => {
         size += chunk.length;
-        console.log(Math.floor((size / data.size) * 100) + "%");
+        console.log(Math.floor((size / data.size) * 100) + '%');
         fileBuffer.push(chunk);
       });
-      stream.on("end", () => {
+      stream.on('end', () => {
         const BufferData = Buffer.concat(fileBuffer);
         uploadFileToAws(BufferData, data.name, user.name);
         /* TODO 以上會在上傳到aws，上傳前直接在前端把圖檔preview就好，以下可以不用作
@@ -345,13 +329,13 @@ if (cluster.isMaster) {
       // });
       callback && callback();
     });
-    socket.on("disconnect", () => {
-      console.log("disconnect!!!");
+    socket.on('disconnect', () => {
+      console.log('disconnect!!!');
       const user = removeUser(socket.id);
       if (user) {
-        io.to(user.room).emit("message", {
-          user: "admin",
-          text: `${user.name} has left.`,
+        io.to(user.room).emit('message', {
+          user: 'admin',
+          text: `${user.name} has left.`
         });
       }
     });
@@ -363,17 +347,24 @@ if (cluster.isMaster) {
 
   // error handler middleware
   app.use((error, req, res, next) => {
-    console.log("這裏", error, "===", error.status, "===");
+    // console.log('這裏', error, '===', error.status, '===');
     res.status(error.status || 500).send({
       error: {
         status: error.status || 500,
-        message: error.message || "Internal Server Error",
-      },
+        message: error.message || 'Internal Server Error'
+      }
     });
   });
 
   server.listen(PORT, () => {
-    console.log("2=", PORT);
     console.log(`listening ${PORT}`);
   });
+  process.exit();
+};
+
+// is the file being executed in mster mode?
+if (cluster.isMaster) {
+  masterProcess();
+} else {
+  childProcess();
 }

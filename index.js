fcc6e4e329d3635656cf9f2f3f85a277a80c2716
beck <stevenaura@yahoo.com.tw>
2020-10-15 19:23:59 +0800

    fix socketio

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ const childProcess = () => {
     })
     .catch((err) => {
       console.error("Connection error", err);
-      process.exit();
+      // process.exit();
     });
 
   const initial = () => {
@@ -155,90 +155,97 @@ const childProcess = () => {
   // });
 
   const { addUser, getUser, removeUser, getUsersInRoom } = require("./users");
-  io.on("connection", (socket) => {
-    console.log("we have connection!!!");
+  io.on(
+    "connection",
+    {
+      path: "/socket.io",
+      transports: ["websocket"],
+      secure: true,
+    },
+    (socket) => {
+      console.log("we have connection!!!");
 
-    // 方法零 一次整個傳輸
-    //   fs.readFile(__dirname + '/images/img1.jpg', function(err, buf) {
-    //     console.log(buf);
-    //     socket.emit('image', { image: true, buffer: buf.toString('base64') });
-    //     console.log('image file is initialized');
-    //   });
+      // 方法零 一次整個傳輸
+      //   fs.readFile(__dirname + '/images/img1.jpg', function(err, buf) {
+      //     console.log(buf);
+      //     socket.emit('image', { image: true, buffer: buf.toString('base64') });
+      //     console.log('image file is initialized');
+      //   });
 
-    // 方法一利用pipepipe就是寫進去的動作
-    // let readableStream = fs.createReadStream('./images/img2.jpg');
-    // let writableStream = fs.createWriteStream('./images/testimg2.jpg');
-    // readableStream.on('data', function (chunk) {
-    //   console.log(chunk);
-    // })
-    // readableStream.pipe(writableStream);
-    // readableStream.on('end', function(chunk) {
-    //   console.log('結束');
-    // });
+      // 方法一利用pipepipe就是寫進去的動作
+      // let readableStream = fs.createReadStream('./images/img2.jpg');
+      // let writableStream = fs.createWriteStream('./images/testimg2.jpg');
+      // readableStream.on('data', function (chunk) {
+      //   console.log(chunk);
+      // })
+      // readableStream.pipe(writableStream);
+      // readableStream.on('end', function(chunk) {
+      //   console.log('結束');
+      // });
 
-    // 方法二
-    // var sourceStream = fs.createReadStream('./images/img1.jpg');
-    // var sinkStream = fs.createWriteStream('./images/testimg1.jpg');
-    // sourceStream.on('data', function (chunk) {
-    //   console.log(chunk);
-    //   sinkStream.write(chunk);
-    // }).on('end', function () {
-    //   sinkStream.end();
-    // });
+      // 方法二
+      // var sourceStream = fs.createReadStream('./images/img1.jpg');
+      // var sinkStream = fs.createWriteStream('./images/testimg1.jpg');
+      // sourceStream.on('data', function (chunk) {
+      //   console.log(chunk);
+      //   sinkStream.write(chunk);
+      // }).on('end', function () {
+      //   sinkStream.end();
+      // });
 
-    //來玩玩socket.io-stream
-    // ss(socket).on('sendFile', function(stream, data) {
-    //   console.log('data', data);
-    //   var filename = path.basename(data.name);
-    //   stream.pipe(fs.createWriteStream(filename));
-    // });
+      //來玩玩socket.io-stream
+      // ss(socket).on('sendFile', function(stream, data) {
+      //   console.log('data', data);
+      //   var filename = path.basename(data.name);
+      //   stream.pipe(fs.createWriteStream(filename));
+      // });
 
-    socket.on("join", ({ name, room }, errorCallback) => {
-      const { error, user } = addUser({ id: socket.id, name, room });
-      if (error) return errorCallback(error);
-      socket.join(user.room);
-      socket.emit("message", {
-        user: "admin",
-        text: `${user.name}, welcome to the room ${user.room}`,
-      });
-
-      socket.emit("image", {
-        user: "admin",
-        text: `${user.name}, welcome to the room ${user.room}`,
-      });
-
-      // broadcast: send message to everyone besides to that user
-      socket.broadcast
-        .to(user.room)
-        .emit("message", { user: "admin", text: `${user.name} has joined!` });
-      //要查
-      io.to(user.room).emit("roomData", {
-        room: user.room,
-        users: getUsersInRoom(user.room),
-      });
-      errorCallback();
-    });
-    socket.on("sendMessage", (message, callback) => {
-      const user = getUser(socket.id);
-      const isGoogleTyping = message.includes("@gg=");
-      if (isGoogleTyping) {
-        const destination = message.split("@gg=").pop();
-        const addressDom = `<a target="blank" href='https://www.google.com.tw/maps/search/${destination}'>${destination}</a>`;
-        io.to(user.room).emit("message", {
-          user: user.name,
-          text: null,
-          address: addressDom,
+      socket.on("join", ({ name, room }, errorCallback) => {
+        const { error, user } = addUser({ id: socket.id, name, room });
+        if (error) return errorCallback(error);
+        socket.join(user.room);
+        socket.emit("message", {
+          user: "admin",
+          text: `${user.name}, welcome to the room ${user.room}`,
         });
-        callback(); //奇怪
-        return;
-      }
-      //io.to要查
-      io.to(user.room).emit("message", { user: user.name, text: message });
-      // io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
-      callback(); //奇怪
-    });
 
-    /* 可用的snippet
+        socket.emit("image", {
+          user: "admin",
+          text: `${user.name}, welcome to the room ${user.room}`,
+        });
+
+        // broadcast: send message to everyone besides to that user
+        socket.broadcast
+          .to(user.room)
+          .emit("message", { user: "admin", text: `${user.name} has joined!` });
+        //要查
+        io.to(user.room).emit("roomData", {
+          room: user.room,
+          users: getUsersInRoom(user.room),
+        });
+        errorCallback();
+      });
+      socket.on("sendMessage", (message, callback) => {
+        const user = getUser(socket.id);
+        const isGoogleTyping = message.includes("@gg=");
+        if (isGoogleTyping) {
+          const destination = message.split("@gg=").pop();
+          const addressDom = `<a target="blank" href='https://www.google.com.tw/maps/search/${destination}'>${destination}</a>`;
+          io.to(user.room).emit("message", {
+            user: user.name,
+            text: null,
+            address: addressDom,
+          });
+          callback(); //奇怪
+          return;
+        }
+        //io.to要查
+        io.to(user.room).emit("message", { user: user.name, text: message });
+        // io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+        callback(); //奇怪
+      });
+
+      /* 可用的snippet
   socket.on('sendFile', (file, data, callback) => {
     const user = getUser(socket.id);
     //io.to要查
@@ -253,93 +260,94 @@ const childProcess = () => {
   });
   */
 
-    const uploadFileToAws = (bufferData, fileName, userName) => {
-      const params = {
-        Bucket: "easychat", // pass your bucket name
-        Key: fileName, // file will be saved as testBucket/contacts.csv
-        Body: bufferData, //JSON.stringify(data, null, 2)
+      const uploadFileToAws = (bufferData, fileName, userName) => {
+        const params = {
+          Bucket: "easychat", // pass your bucket name
+          Key: fileName, // file will be saved as testBucket/contacts.csv
+          Body: bufferData, //JSON.stringify(data, null, 2)
+        };
+        s3.upload(params, function (s3Err, data) {
+          if (s3Err) throw s3Err;
+          console.log(`上傳成功位子在 ${data.Location}`);
+        });
       };
-      s3.upload(params, function (s3Err, data) {
-        if (s3Err) throw s3Err;
-        console.log(`上傳成功位子在 ${data.Location}`);
-      });
-    };
 
-    ss(socket).on("sendFile", (stream, data, callback) => {
-      const user = getUser(socket.id);
-      //io.to要查
-      // const filename = path.basename(data.name);
-      let size = 0;
-      let fileBuffer = [];
-      stream.on("data", (chunk) => {
-        size += chunk.length;
-        console.log(Math.floor((size / data.size) * 100) + "%");
-        fileBuffer.push(chunk);
-      });
-      stream.on("end", () => {
-        /* TODO 以上會在上傳到aws，上傳前直接在前端把圖檔preview就好，以下可以不用作
-    右邊為轉成webP技巧網站 https://css-tricks.com/using-webp-images/ */
-        // stream.on("end", () => {
-        const sentFile = Buffer.concat(fileBuffer).toString("base64");
-        io.to(user.room).emit("file", {
-          user: user.name,
-          upload: sentFile,
-          type: data.type,
+      ss(socket).on("sendFile", (stream, data, callback) => {
+        const user = getUser(socket.id);
+        //io.to要查
+        // const filename = path.basename(data.name);
+        let size = 0;
+        let fileBuffer = [];
+        stream.on("data", (chunk) => {
+          size += chunk.length;
+          console.log(Math.floor((size / data.size) * 100) + "%");
+          fileBuffer.push(chunk);
         });
-      });
-
-      // stream.pipe(fs.createWriteStream(filename));
-
-      // });
-      //來玩玩socket.io-stream
-      // ss(socket).on('sendFile', function(stream, data) {
-      //   console.log('data', data);
-      //   var filename = path.basename(data.name);
-      //   stream.pipe(fs.createWriteStream(filename));
-      // });
-      callback && callback();
-    });
-
-    ss(socket).on("uploadFile", (stream, data, callback) => {
-      const user = getUser(socket.id);
-      //io.to要查
-      // const filename = path.basename(data.name);
-      let size = 0;
-      let fileBuffer = [];
-      stream.on("data", (chunk) => {
-        size += chunk.length;
-        console.log(Math.floor((size / data.size) * 100) + "%");
-        fileBuffer.push(chunk);
-      });
-      stream.on("end", () => {
-        const BufferData = Buffer.concat(fileBuffer);
-        uploadFileToAws(BufferData, data.name, user.name);
-        /* TODO 以上會在上傳到aws，上傳前直接在前端把圖檔preview就好，以下可以不用作
+        stream.on("end", () => {
+          /* TODO 以上會在上傳到aws，上傳前直接在前端把圖檔preview就好，以下可以不用作
     右邊為轉成webP技巧網站 https://css-tricks.com/using-webp-images/ */
+          // stream.on("end", () => {
+          const sentFile = Buffer.concat(fileBuffer).toString("base64");
+          io.to(user.room).emit("file", {
+            user: user.name,
+            upload: sentFile,
+            type: data.type,
+          });
+        });
+
+        // stream.pipe(fs.createWriteStream(filename));
+
+        // });
+        //來玩玩socket.io-stream
+        // ss(socket).on('sendFile', function(stream, data) {
+        //   console.log('data', data);
+        //   var filename = path.basename(data.name);
+        //   stream.pipe(fs.createWriteStream(filename));
+        // });
+        callback && callback();
       });
 
-      // stream.pipe(fs.createWriteStream(filename));
-
-      // });
-      //來玩玩socket.io-stream
-      // ss(socket).on('sendFile', function(stream, data) {
-      //   console.log('data', data);
-      //   var filename = path.basename(data.name);
-      //   stream.pipe(fs.createWriteStream(filename));
-      // });
-      callback && callback();
-    });
-    socket.on("disconnect", () => {
-      console.log("disconnect!!!");
-      const user = removeUser(socket.id);
-      if (user) {
-        io.to(user.room).emit("message", {
-          user: "admin",
-          text: `${user.name} has left.`,
+      ss(socket).on("uploadFile", (stream, data, callback) => {
+        const user = getUser(socket.id);
+        //io.to要查
+        // const filename = path.basename(data.name);
+        let size = 0;
+        let fileBuffer = [];
+        stream.on("data", (chunk) => {
+          size += chunk.length;
+          console.log(Math.floor((size / data.size) * 100) + "%");
+          fileBuffer.push(chunk);
         });
-      }
-    });
-  });
+        stream.on("end", () => {
+          const BufferData = Buffer.concat(fileBuffer);
+          uploadFileToAws(BufferData, data.name, user.name);
+          /* TODO 以上會在上傳到aws，上傳前直接在前端把圖檔preview就好，以下可以不用作
+    右邊為轉成webP技巧網站 https://css-tricks.com/using-webp-images/ */
+        });
+
+        // stream.pipe(fs.createWriteStream(filename));
+
+        // });
+        //來玩玩socket.io-stream
+        // ss(socket).on('sendFile', function(stream, data) {
+        //   console.log('data', data);
+        //   var filename = path.basename(data.name);
+        //   stream.pipe(fs.createWriteStream(filename));
+        // });
+        callback && callback();
+      });
+      socket.on("disconnect", () => {
+        console.log("disconnect!!!");
+        const user = removeUser(socket.id);
+        if (user) {
+          io.to(user.room).emit("message", {
+            user: "admin",
+            text: `${user.name} has left.`,
+          });
+        }
+      });
+    }
+  );
 
   app.use((req, res, next) => {
     next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));

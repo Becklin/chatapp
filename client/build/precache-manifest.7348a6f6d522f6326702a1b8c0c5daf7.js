a16c151263da037cf33179f1ad41b4e575fa64b6
BeckLin <beck.lin@ISTWLAPAPPLE2029.local>
2020-10-29 16:00:25 +0800

    run build

---
+++ client/build/precache-manifest.7348a6f6d522f6326702a1b8c0c5daf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaeccc39a00c2b17c88a5cba392c4b49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab498366882cfe8cfdc5",
+    "url": "/static/css/main.e118cbde.chunk.css"
+  },
+  {
+    "revision": "def9b364d60721efcfd6",
+    "url": "/static/js/2.5ffcc453.chunk.js"
+  },
+  {
+    "revision": "7f83455c56d75fbf13c579d8e322cfcf",
+    "url": "/static/js/2.5ffcc453.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab498366882cfe8cfdc5",
+    "url": "/static/js/main.b92bbd69.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d480d374dd4d4ffd1231b0c89d35259d.js"
+  "/precache-manifest.7348a6f6d522f6326702a1b8c0c5daf7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
